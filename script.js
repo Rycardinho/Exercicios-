eafa649b2d7704153594eb6d220af6a58e96adb8
Ricardo Braz <106495830+Rycardinho@users.noreply.github.com>
2022-08-02 20:04:59 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-alert ('olá')
+function carregar () {
+
+let msg = window.document.getElementById('msg')
+let img = window.document.getElementById('imagem')
+let data = new Date()
+//let hora = data.getHours()
+let hora = 20
+
+    if (hora >= 0 && hora < 12) {
+        // bom dia
+        img.src = "manhã.png"
+        msg.innerHTML = `Agora são ${hora} horas, Bom Dia!`
+        document.body.style.background = '#87CEEB'
+    } else if (hora >= 12 && hora <18) {
+        //boa tarde
+        img.src = "tarde.png"
+        msg.innerHTML = `Agora são ${hora} horas, Boa Tarde!`
+        document.body.style.background = '#D2691E'
+    } else {
+        // boa noite 
+        img.src = "noite.png"
+        msg.innerHTML = `Agora são ${hora} horas, Boa Noite!`
+        document.body.style.background = '#515154'
+    }
+
+
+} 
+
+
+
